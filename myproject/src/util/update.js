1611b45a18e4049fbb8399b5724eb34af1f43da1
wangjian1023 <31922854+wangjian1023@users.noreply.github.com>
2017-11-01 20:54:28 +0800

    修改版2.0

--- myproject/src/util/update.js
+++ myproject/src/util/update.js
@@ -5,42 +5,105 @@ module.exports = [
     recDescribe: '最近朋友从家里带来不少小土豆，就来烧炖排骨的',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '您发低温覅恩我覅诶我能覅微博覅并未U币覅保卫部分'
   }, {
     recName: '羊肉白萝卜汤',
     recPhoto: 'http://i04.pic.sogou.com/b1cb400961f24a02',
     recDescribe: '',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无'
   }, {
     recName: '烤培根金针菇卷',
     recPhoto: 'http://i02.pic.sogou.com/03d1234a351e628b',
     recDescribe: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无'
   }, {
     recName: '芒果千层蛋糕',
     recPhoto: 'http://i04.pictn.sogoucdn.com/b67f0e350d029c5f',
     recDescribe: '',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无'
   }, {
     recName: '东北酱骨棒',
     recPhoto: 'http://i02.pic.sogou.com/6402095241ac64e8',
     recDescribe: '天气转凉，就到了肉食爱好者的季节，从入味酥软却不柴的肉，脆滑而非常有口感的肉筋，再到喷香的骨髓',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无'
   }, {
     recName: '芝麻脆蛋卷 #MEYER,焕心厨房,唤醒美味#',
     recPhoto: 'http://i02.pic.sogou.com/3c35f7ff8ee1df74',
     recDescribe: '大家都知道做这个蛋卷没有蛋卷机也必须要有一口好的平底锅，不粘效果极其重要。能把芝麻蛋卷做的又香又脆',
     recView: '1344人浏览',
     hierarchy: '初级',
-    time: '30'
+    time: '30',
+    food: [
+      {materials: '牛腩', conversion: '500克'},
+      {materials: '白萝卜', conversion: '500克'}
+    ],
+    summary: [
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' },
+      { artist: 'http://i02.pic.sogou.com/f5a123aed7962b74', character: '很简单做哦，用培根卷着金针菇，再刷点烤肉酱，烤箱烤十几分钟就可以吃啦，没有烤箱用平底锅煎着吃也不错!' }
+    ],
+    reminder: '呜呜呜呜无无无无无无无无无无无无无无无无无无无无无无无'
   }
 
 ]
